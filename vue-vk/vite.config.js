b9e95698a55f719a12ae9f305ff4646e41528524
tsirkulyar <106337353+Tsirkulyar@users.noreply.github.com>
2023-11-12 16:01:53 +0300

    Update vite.config.js

--- vue-vk/vite.config.js
+++ vue-vk/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/uno-soft`,
+  base: `/uno-softVK`,
   server: {
     proxy: {
       "/api": {
